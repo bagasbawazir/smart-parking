5ee8408bf1fe1cc6c17bd592a28c5ce628f5519c
Bawazir Fadhil Mohammad <bawazirfadhil@gmail.com>
2020-10-06 11:57:58 +0700

    Update database.py

--- webservice/database.py
+++ webservice/database.py
@@ -1,7 +1,6 @@
 import MySQLdb
 import MySQLdb.cursors
 
-db = MySQLdb.connect(
-    host="localhost", user="root", passwd="", db="smart-parking")
+db = MySQLdb.connect(host="localhost", user="root", passwd="", db="smart-parking")
 
 cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
